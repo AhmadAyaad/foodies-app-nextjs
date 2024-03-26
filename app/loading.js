b89c02716fddacc7deeaaa938aad404974ca1732
Ahmed Mohamed Ayad <aayad@RASAN.CO>
2024-03-26 17:01:20 +0200

    add loading page

---
+++ app/loading.js
@@ -0,0 +1,8 @@
+import classes from './loading.module.css';
+function MealsLoadingPage() {
+    return ( 
+        <p className={classes.loading}> Meals is loading ...</p>
+     );
+}
+
+export default MealsLoadingPage;
